68a0a73aff813d5ebdf0a5ffbacadb90042d0581
Daniel L <43019017+funkedee@users.noreply.github.com>
2019-03-10 12:29:36 -0600

    build

---
+++ build/precache-manifest.2fa48937f77be6340b8075c9dcec0d07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b84fe2840af4d05cf6b0",
+    "url": "/static/js/main.b84fe284.chunk.js"
+  },
+  {
+    "revision": "10c9f4f9fed65ea7dbe6",
+    "url": "/static/js/2.10c9f4f9.chunk.js"
+  },
+  {
+    "revision": "b84fe2840af4d05cf6b0",
+    "url": "/static/css/main.0af5f85a.chunk.css"
+  },
+  {
+    "revision": "b28446f83c897ef7b6eb7bd10290d334",
+    "url": "/index.html"
+  }
+];
